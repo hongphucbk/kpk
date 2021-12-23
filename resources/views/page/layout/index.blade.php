64567cce92e835a0d0e2327b038c1924c4dea5f5
phuctruong <phuchong94@gmail.com>
2021-12-24 00:39:48 +0700

    add icon

--- resources/views/page/layout/index.blade.php
+++ resources/views/page/layout/index.blade.php
@@ -3,6 +3,8 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="v1/image/kpk.png" type="image/icon type">
+
     <title>KPK Digital Transformation | @yield('title')</title>
     <link rel="stylesheet" href="v1/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
